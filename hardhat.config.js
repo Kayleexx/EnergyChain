283c491def408fd09034132eee3a6e28a1638941
Mitali Sinha <105365766+Kayleexx@users.noreply.github.com>
2023-11-30 03:05:09 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,7 +7,7 @@ module.exports = {
   networks: {
     mumbai: {
       url: "https://rpc-mumbai.maticvigil.com/",
-      accounts: ['1bab485d8a765e6ecad8403f8362f0bbb0e3f3ad8ce69df4ce05d27ff6b27262'],
+      accounts: ['YOUR_PRIVATE_KEY'],
     },
   },
 };
